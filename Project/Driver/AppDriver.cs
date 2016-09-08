4611d4be1ca36f9ca9b2abf984e24161c06b94e4
石川達也 <stone.guitar.777@gmail.com>
2016-09-09 08:58:34 +0900

    テスト用のプロジェクト追加

---
+++ Project/Driver/AppDriver.cs
@@ -0,0 +1,37 @@
+﻿using Codeer.Friendly.Windows;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Driver
+{
+    public class AppDriver : IDisposable
+    {
+        public Process Process { get; private set; }
+        WindowsAppFriend _app;
+
+        public AppDriver()
+        {
+            if (Process != null)
+            {
+                return;
+            }
+            var dir = Path.GetFullPath("../../../WpfApplication/bin/Release");
+            var pathExe = dir + "/WpfApplication.exe";
+            var info = new ProcessStartInfo(pathExe) { WorkingDirectory = dir };
+            Process = Process.Start(pathExe);
+            _app = new WindowsAppFriend(Process);
+        }
+
+        public void Dispose()
+        {
+            _app.Dispose();
+            try
+            {
+                Process.Kill();
+            }
+            catch { }
+            Process = null;
+        }
+    }
+}
---
+++ Project/Scenario/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Driver;
+
+namespace Scenario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        AppDriver _app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _app = new AppDriver();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _app.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
